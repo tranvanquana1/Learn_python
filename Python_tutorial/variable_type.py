fb589a15e8ba4c1cf78cf597126b3597e5d99409
tranvanquana1 <42964818+tranvanquana1@users.noreply.github.com>
2020-04-11 23:01:54 +0700

    variable and type

---
+++ Python_tutorial/variable_type.py
@@ -0,0 +1,35 @@
+counter = 10    #integer
+miles = 100.0   #float
+name = 'tit'    #string
+
+print(counter)
+print(miles)
+print(name)
+#nhieu bien gan 1 gia tri
+a=b=c=1
+#gan nhieu doi tuong cho nhieu bien
+d, e, f = 1, 10.0, 'tit'
+#xoa bien a, b, c
+del a, b, c
+#co 4 kieu number int, long, float, complex
+myint= 16
+#mylong = 0122L
+myfloat = 15.0
+mycomplex = 1+3.14j
+
+print(mycomplex)
+
+#string
+str = 'hello world'
+
+#list
+list = [1, 2, 'hello']
+print(list)
+
+#tuple
+tuple = ('abc', 70, 3.14, 'tit')
+print(tuple)
+
+#dictionary
+dictionary = {'name': 'tit', 'age': 1, 'city': 'ha noi'}
+print(dictionary)
