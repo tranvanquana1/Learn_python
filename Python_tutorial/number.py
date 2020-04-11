1060883e62d44bab06a129c6658a7f0b913cf860
tranvanquana1 <42964818+tranvanquana1@users.noreply.github.com>
2020-04-12 00:42:23 +0700

    number

---
+++ Python_tutorial/number.py
@@ -0,0 +1,13 @@
+#ep kieu
+x, y = 10, 3
+int(x)
+float(x)
+#long(x)
+complex(x, y)
+
+#cac ham toan hoc
+abs(x)  #tri tuyet doi
+max(x, y)   #tim max
+min(x, y)   #tim min
+pow(x, y)   #ham mu
+round(0.5)    #lam tron
