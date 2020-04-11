584e945be4d8dc4d595bb74eb84d8d44755a2e77
tranvanquana1 <42964818+tranvanquana1@users.noreply.github.com>
2020-04-11 23:45:25 +0700

    decision making  and loop

---
+++ Python_tutorial/decision_loop.py
@@ -0,0 +1,26 @@
+#lenh if else
+number = 100
+if (number>100):
+    print('lon hon 100')
+elif (number<100):
+    print('nho hon 100')
+else:
+    print('bang 100')
+
+#vong lap for
+for i in range(10):
+    print(i)
+
+#vong lap while
+index = 0
+while (index < 10):
+    print(index)
+    index+=1
+
+#lenh break
+ind = 0
+while (ind < 10):
+    if (ind == 5):
+        break
+    print(ind)
+    ind+=1
