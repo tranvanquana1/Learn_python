ef3ee40eaec0ea84ed9b718c59a4405a0d8c6a17
tranvanquana1 <42964818+tranvanquana1@users.noreply.github.com>
2020-04-11 23:32:49 +0700

    operator

---
+++ Python_tutorial/operator.py
@@ -0,0 +1,24 @@
+#tinh toan so hoc
+a = 10
+b = 20
+
+print(a+b)  #cong 
+print(b-a)  #tru
+print(a*b)  #nhan
+print(b/a)  #chia lay nguyen
+print(b%a)  #chia lay du
+print(b**a) #lay mu
+
+#phep so sanh co ==, >, <, >=, <=, !=
+print(a==b)
+
+#so sanh chuoi
+a = 'tit'
+b = 't'
+
+print(b in a)
+print(b not in a)
+
+#kiem tra vi tri o nho
+print(b is a)
+print(b is not a)
