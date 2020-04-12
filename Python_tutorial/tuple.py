bfb82b6ea489b6ce8f30944df513d78c44e88b92
tranvanquana1 <42964818+tranvanquana1@users.noreply.github.com>
2020-04-12 16:40:08 +0700

    tuple

---
+++ Python_tutorial/tuple.py
@@ -0,0 +1,31 @@
+tuple1 = ('Tit', 13, 16)
+tuple2 = (1, 2, 3, 4, 5)
+tuple3 = 'a', 'b', 'c', 'd', 'e'
+
+print(tuple3)
+
+#khai bao tuple
+tuple = ()
+
+tuple4 = ('Tit', )  #1 phan tu phai co dau , 
+
+tuple1[1]
+tuple2[1:3]
+
+#tuple la bat bien nen khong the thay doi phan tu, them va xoa phan tu
+
+#tuple1[1] = 100     #false
+
+#cong tuple
+tuple = tuple2 + tuple3
+print(tuple)
+
+#xoa toan bo du lieu tuple
+del tuple   
+
+#cac ham: 
+len(tuple1)
+max(tuple2)
+min(tuple2)
+tuple(['tit', 13, 16])
+print(tuple)
