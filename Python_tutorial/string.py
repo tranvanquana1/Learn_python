b52e05cc3d7ef8884a3768a9dcdb7231a1cea618
tranvanquana1 <42964818+tranvanquana1@users.noreply.github.com>
2020-04-12 10:12:55 +0700

    string

---
+++ Python_tutorial/string.py
@@ -0,0 +1,18 @@
+str = 'Hello world'
+
+print('str[0]:', str[0])
+print('str[1:5]:', str[1:5])
+
+print('update string:',str[:6] + 'python')
+
+#cac phep toan tren string: +, *, [:], in, not in
+print('h' in str)
+print(str*2)
+
+#toan tu dinh dang chuoi %d, %s, %f, %c
+print('My name is %s and weight is %d kg' %('Tit', 10))
+
+#chuoi in hoa, chuoi in thuong
+print(str.upper())
+print(str.lower())
+
